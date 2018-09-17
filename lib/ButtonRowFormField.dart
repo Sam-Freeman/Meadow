d74c9433e82c07c79af7e6ac64745feeb56ce92c
Samuel Freeman <freeman@d-tt.nl>
2018-09-18 00:12:59 +0200

    Fixed width of buttons

--- lib/ButtonRowFormField.dart
+++ lib/ButtonRowFormField.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
 
+Widget _buildButton({
+    String textValue,
+    double width,
+    FormFieldState<String> state,
+    Color selectedColor = Colors.black12,
+    Color defaultColor = Colors.white,
+    Color selectedBorderColor = Colors.black,
+    Color defaultBorderColor = Colors.black45,
+}) {
+    return InkWell(
+        onTap: () => state.didChange(textValue),
+        child: Container(
+            decoration: BoxDecoration(
+                color: (state.value == textValue) ? selectedColor : defaultColor,
+                borderRadius: BorderRadius.circular(8.0),
+                border: (state.value == textValue) ? Border.all(color: selectedBorderColor, width: 2.0) : Border.all(color: defaultBorderColor, width: 1.0),
+            ),
+            width: width,
+            alignment: Alignment.center,
+            child: Text(textValue),
+        ),
+    );
+}
+
 class ButtonRowFormField extends FormField<String> {
 
-    static Widget _buildButton({
-        String textValue,
-        double width,
-        FormFieldState<String> state,
-        Color selectedColor = Colors.black12,
-        Color defaultColor = Colors.white,
-        Color selectedBorderColor = Colors.black,
-        Color defaultBorderColor = Colors.black45,
-    }) {
-        return InkWell(
-            onTap: () => state.didChange(textValue),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: (state.value == textValue) ? selectedColor : defaultColor,
-                    borderRadius: BorderRadius.circular(8.0),
-                    border: (state.value == textValue) ? Border.all(color: selectedBorderColor, width: 2.0) : Border.all(color: defaultBorderColor, width: 1.0),
-                ),
-                width: width,
-                alignment: Alignment.center,
-                child: Text(textValue),
-            ),
-        );
-    }
-
     ButtonRowFormField({
+        @required double rowWidth,
         @required List<String> buttonValues,
         String title,
         Icon icon,
-        double spaceBetweenButtons = 30.0,
+        double spaceBetweenButtons = 20.0,
         double buttonHeight = 50.0,
         FormFieldSetter<String> onSaved,
         FormFieldValidator<String> validator,
@@ -51,8 +52,7 @@ class ButtonRowFormField extends FormField<String> {
                             children: [
                                 _buildButton(
                                     textValue: buttonValues[index],
-                                    // Todo: fix the width based on the screen size
-                                    width: 80.0,
+                                    width: (rowWidth - (buttonValues.length * spaceBetweenButtons)) / (buttonValues.length),
                                     state: state
                                 ),
                                 SizedBox(width: (index != buttonValues.length - 1) ? spaceBetweenButtons : 0.0,)
